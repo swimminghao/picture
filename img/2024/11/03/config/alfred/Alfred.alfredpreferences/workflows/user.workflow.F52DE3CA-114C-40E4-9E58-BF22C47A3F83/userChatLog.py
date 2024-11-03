4034c9dd1fec5f3fdae8d4d5dfaec951762435bc
swimminghao <34031308+swimminghao@users.noreply.github.com>
2024-11-03 18:35:53 +0800

    ⬆ Uploaded by uPic 👉❤️ Powered by https://github.com/gee1k/uPic ❤️👈

---
+++ img/2024/11/03/config/alfred/Alfred.alfredpreferences/workflows/user.workflow.F52DE3CA-114C-40E4-9E58-BF22C47A3F83/userChatLog.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+import json,sys,os
+from workflow import Workflow, web
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def main(wf):
+    userId = os.getenv('userId')
+    baseUrl = os.getenv('baseUrl')
+    url = baseUrl + 'chatlog?userId=' + userId + '&count=45'
+    try:
+        result = web.get(url=url)
+        result.raise_for_status()
+        resp = result.text
+        userList = json.loads(resp)
+        if len(userList) > 0:
+            wf.store_data('wechat_send_content',sys.argv[1])
+            for item in userList:
+                title = item['title']
+                subtitle = item['subTitle']
+                icon = item['icon']
+                userId = item['userId']
+                copyText = item['copyText']
+                qlurl = item['url']
+                srvId = str(item['srvId'])
+                l = len(title)
+                lineNun = 70
+                if l < lineNun:
+                    largetext = title
+                else:
+                    b = []
+                    for n in range(l):
+                        if n % lineNun == 0:
+                            b.append(title[n:n+lineNun])
+                    largetext='\n'.join(b)
+                wf.add_item(title=title, subtitle=subtitle, icon=icon, valid=True, largetext=largetext, quicklookurl=qlurl, copytext=copyText, arg=srvId)
+        else:
+            wf.add_item(title='找不到联系人…',subtitle='请重新输入')
+    except IOError:
+                wf.add_item(title='请先启动微信 & 登录…',subtitle='并确保安装微信小助手')
+
+    wf.send_feedback()
+if __name__ == '__main__':
+    wf = Workflow()
+    sys.exit(wf.run(main))
